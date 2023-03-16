3eca5a91477513b7dc4f479369ac114ad478f72a
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-03-16 17:13:04 +0300

    edit the logical code

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 
 let user = prompt("What is your name ?");
-alert("Hello, " + user + "!");
+
 
 let gender = prompt("What is your gender ?");
 if(gender == "Male" || gender == "male"){
-    alert("Welcome Mr "+user);
+    alert("Hello Mr " + user + " !");
 } else if (gender == "Female" || gender == "female"){
-    alert("Welcome Ms "+user)
+    alert("Hello Ms " + user + " !");
 }else {
     alert("you are welcome");
 }
 
+let order; 
 let drink = prompt("What would you like to drink? hot or cold drink ? ");
 if(drink == "Hot" || drink == "hot"){
-    prompt("enter the name of drink you want :")
+    order = prompt("enter the name of drink you want :")
 }else if (drink == "Cold" || drink == "cold"){
-    prompt("enter the name of drink you want :")
+   order = prompt("enter the name of drink you want :")
    
 }
 
 alert("your order is perparing right now")
 
-alert("Name :"+ user + " , The order :" + drink)
+alert("Name : "+ user + " , Your order : " + order)
