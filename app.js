e91d14700bd7a99f25acdac6040918e800cc30f9
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-03-17 19:23:15 +0300

    edit the files

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ let user = prompt("What is your name ?");
 
 
 let gender = prompt("What is your gender ?");
-if(gender == "Male" || gender == "male"){
+if (gender == "Male" || gender == "male") {
     alert("Hello Mr " + user + " !");
-} else if (gender == "Female" || gender == "female"){
+} else if (gender == "Female" || gender == "female") {
     alert("Hello Ms " + user + " !");
-}else {
+} else {
     alert("you are welcome");
 }
 
-let order; 
+let order;
 let drink = prompt("What would you like to drink? hot or cold drink ? ");
-if(drink == "Hot" || drink == "hot"){
+if (drink == "Hot" || drink == "hot") {
     order = prompt("enter the name of drink you want :")
-}else if (drink == "Cold" || drink == "cold"){
-   order = prompt("enter the name of drink you want :")
-   
+} else if (drink == "Cold" || drink == "cold") {
+    order = prompt("enter the name of drink you want :")
+
 }
 
 alert("your order is perparing right now")
 
-alert("Name : "+ user + " , Your order : " + order)
+alert("Name : " + user + " , Your order : " + order)
