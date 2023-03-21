e4585f9899cc74da4d50724fa584425b0887cbe6
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-03-21 06:03:49 +0300

    add new branch with function

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-
 let username = prompt("What is your name ?");
 
 
-let gender = prompt("What is your gender ?");
-while (gender != "Male" && gender != "male" && gender != "female" && gender != "Female") {
+// let gender = prompt("What is your gender ?");
+// while (gender != "Male" && gender != "male" && gender != "female" && gender != "Female") {
 
-    gender = prompt("Please re-enter your gender !")
+//     gender = prompt("Please re-enter your gender !")
+// }
+let gender = prompt("Please enter your gender");
 
+function genderName(gender1) {
+    while (gender1 != "Male" && gender1 != "male" && gender1 != "female" && gender1 != "Female") {
+        gender1 = prompt("Please enter your gender correctly :")
+    }
 
-
+    if (gender1 == "Male" || gender1 == "male") {
+        alert("Hello Mr " + username + " !");
+    } else if (gender1 == "Female" || gender1 == "female") {
+        alert("Hello Ms " + username + " !")
+    }
 }
 
-if (gender == "Male" || gender == "male") {
-    alert("Hello Mr " + username + " !");
-} else {
-    alert("Hello Ms " + username + " !")
-}
-
+genderName(gender)
 
 
 let drinkName;
@@ -29,10 +33,13 @@ if (typeOfDrink == "Hot" || typeOfDrink == "hot") {
 
 alert("your order is perparing right now")
 
-// alert("Name : " + username + " , Your order : " + order)
+
+
 let order = [username, gender, typeOfDrink, drinkName]
 
 for (let i = 0; i < order.length; i++) {
     console.log(order[i])
 }
+// alert("Name : " + username + " , Your order : " + order)
+
 
